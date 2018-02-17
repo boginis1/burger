d4ba7ddcdb1ab9d713d6913666e8b28bedf3c7b6
boginis1 <hollyzoba@gmail.com>
2018-02-16 19:17:15 -0700

    Jaws

--- config/connection.js
+++ config/connection.js
@@ -7,6 +7,18 @@ var connection = mysql.createConnection({
   password: "Legend@ry81",
   database: "burgers_db"
 });
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  }else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "hacktheplanet",
+        database: "todoagain_db"
+
+    })
+  };
+
 // Make connection.
 connection.connect(function(err) {
   if (err) {
